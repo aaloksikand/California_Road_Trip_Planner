dd3e87534ef549c299bbd9f397b736773310f1a1
sagegod <cdeng1999@gmail.com>
2022-06-12 23:31:33 -0700

    Mergeing develop

--- assets/js/Modal.js
+++ assets/js/Modal.js
@@ -1,22 +1,4 @@
 
 
-var headers = $('#accordion .accordion-header');
-var contentAreas = $('#accordion .ui-accordion-content ').hide();
-
-
-
-headers.click(function() {
-    var panel = $(this).next();
-    var isOpen = panel.is(':visible');
-    panel[isOpen? 'slideUp': 'slideDown']()
-        .trigger(isOpen? 'hide': 'show');
-    return false;
-});
-
-
-
-
-
-
 
 
